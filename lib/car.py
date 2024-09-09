4a47f3d14f068595d52f515608b39ad0958afcfc
Alvinkimutai <alvinchepkiyeng@gmail.com>
2024-09-09 10:41:16 +0300

    practices on the concept of class introspection

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,17 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
     pass
+
+
+hilux = Car(56, 3)
+
+fielder = Vehicle(35, 2)
+
+print(hilux.go())
+
+print(fielder.go())
+
+print(int.__bases__)
